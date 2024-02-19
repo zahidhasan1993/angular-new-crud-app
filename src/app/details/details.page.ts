071377f73aee6024d47feeda956d08b60577d54c
zahidhasan1993 <officialzahidhasan19932021@gmail.com>
2024-02-19 11:27:29 +0600

    some fixes

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,4 +1,4 @@
-import { Component, Input, inject } from '@angular/core';
+import { Component, Input, OnInit, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
@@ -12,16 +12,22 @@ import { Router } from '@angular/router';
   standalone: true,
   imports: [IonicModule, CommonModule, ReactiveFormsModule],
 })
-export class DetailsPage {
+export class DetailsPage implements OnInit{
   private dataService = inject(ServiceService);
   private router = inject(Router);
-
-  constructor() {}
+  userDetails : any = {};
+  allUsers : any = []
+  constructor() {
+  }
+  ngOnInit(): void {
+    this.allData()
+  }
   @Input() id: number = 0;
   userForm = new FormGroup({
     name: new FormControl(''),
     age: new FormControl(0),
     email: new FormControl(''),
+
     address: new FormControl(''),
   });
 
@@ -31,7 +37,7 @@ export class DetailsPage {
       id: this.id,
     };
     console.log('send data from update data');
-
+``
     await this.dataService.updateData(data);
 
     console.log('updated data');
@@ -39,5 +45,18 @@ export class DetailsPage {
     this.router.navigate(['/']);
 
     // console.log(this.item);
+  }
+
+  async allData() {
+    this.allUsers = await this.dataService.getAllData();
+
+    console.log(this.allUsers);
+    const details = this.allUsers.filter((u:any) => u.id.toString() === this.id.toString());
+
+    this.userDetails = details[0];
+    console.log('from details page :',this.userDetails);
+    
+    
+    
   }
 }
